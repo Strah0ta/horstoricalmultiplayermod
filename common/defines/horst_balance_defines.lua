a6e6cc841924dec3ba8b6f120a835f3d12087ae0
tresimah <pharmercyaddict@gmail.com>
2021-03-05 02:24:21 +0100

    Part 3
    -Pocket Defense lowers infantry defense by 5 and infantry equipment requirements by an additional 3
    -Light 3s are stronger
    -Experienced Soldier losses are 2% vanilla instead of 4% vanilla values

--- common/defines/horst_balance_defines.lua
+++ common/defines/horst_balance_defines.lua
@@ -9,7 +9,7 @@ NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 6		-- Max height of regiments i
 
 NDefines.NMilitary.AIR_SUPPORT_BASE = 0.25                        -- vanilla 0.25 CAS bonus factor for air support moddifier for land unit in combat
 
-NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.04                 -- percentage of experienced solders who die when manpower is removed
+NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.02                 -- percentage of experienced solders who die when manpower is removed
 NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.25                 -- small river crossing
 NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.325           -- large river crossing
 NDefines.NMilitary.ANTI_AIR_ATTACK_TO_AMOUNT = 0.0015 				-- WAS 0.005 | Lowered because vanilla CAS shootdown rates are too high and IC inefficient, but I don't want to buff CAS or air in any other way either ~Thrasymachus | Balancing value to convert equipment stat anti_air_attack to the random % value of airplanes being hit.
