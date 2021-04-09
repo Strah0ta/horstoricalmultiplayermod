4f6f2b18d5e59718a90c87e639164e5fc6fb65e3
tresimah <pharmercyaddict@gmail.com>
2021-04-09 14:24:29 +0200

    Sub changes
    -Submarine death stacks are very easy to kill in combat
    -Subs in combat are much easier to detect

--- common/defines/horst_balance_defines.lua
+++ common/defines/horst_balance_defines.lua
@@ -139,8 +139,8 @@ NDefines.NNavy.NAVALBASE_REPAIR_MULT = 0.1 --vanilla 0.05 repairing a battleship
 NDefines.NNavy.CONVOY_EFFICIENCY_LOSS_MODIFIER = 0.25   --  WAS 1.25, reduced so players have more time to deal with it | How much efficiency drops when losing convoys. If modifier is 0.5, then losing 100% of convoys in short period, the efficiency will drop by 50%.
 NDefines.NNavy.CONVOY_EFFICIENCY_MIN_VALUE = 0.2		-- WAS 0.05, reduced to lower the value of flickering supply to reset supply calculations  | To avoid complete 0% efficiency, set the lower limit.
 
-NDefines.NNavy.NAVAL_COMBAT_SUB_DETECTION_FACTOR = 3.0      -- balance value for sub detection in combat by ships
-
+NDefines.NNavy.NAVAL_COMBAT_SUB_DETECTION_FACTOR = 5.0      -- balance value for sub detection in combat by ships
+NDefines.NNavy.SUBMARINE_REVEAL_ON_MINIMUM_POSITIONING    = 3.0  -- submarine reveal change on 0% positioning
 
 NDefines.NNavy.SUPREMACY_PER_SHIP_BASE = 0                        -- WAS 100 | reduced to relatively nerf subs in terms of naval supremacy, as most of their supremacy comes from a base value of 100
 NDefines.NNavy.SUPREMACY_PER_SHIP_PER_MANPOWER = 0						-- WAS 0.0005 supremacy of a ship is calculated using its IC, manpower and a base define
