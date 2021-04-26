091f4e9bc808f9fa2c4c363c7d02f0568b90a8c2
tresimah <pharmercyaddict@gmail.com>
2021-04-26 18:31:09 +0200

    arabia sports

--- common/defines/horst_balance_defines.lua
+++ common/defines/horst_balance_defines.lua
@@ -10,6 +10,8 @@ NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 6		-- Max height of regiments i
 NDefines.NMilitary.AIR_SUPPORT_BASE = 0.25                        -- vanilla 0.25 CAS bonus factor for air support moddifier for land unit in combat
 NDefines.NMilitary.UNIT_DIGIN_CAP = 3                           -- how "deep" you can dig you can dig in until hitting max bonus
 --NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.025			-- vanilla  0.03	-- Amount of planning lost due to player manual order
+--NDefines.NMilitary.COMBAT_VALUE_ORG_IMPORTANCE = 1		-- VANILLA 1     Multiplier on TotalOrganisation when determining the combat value of a division
+--NDefines.NMilitary.COMBAT_VALUE_STR_IMPORTANCE = 1		-- VANILLA 1      Multiplier on TotalStrength when determining the combat value of a division
 
 NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.01                 -- percentage of experienced solders who die when manpower is removed
 NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.25                 -- small river crossing
