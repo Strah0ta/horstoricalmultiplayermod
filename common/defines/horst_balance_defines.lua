ecc4d2dc90dd91a59286b88d2cf84af9b6aa4de4
tresimah <pharmercyaddict@gmail.com>
2021-05-23 09:25:28 +0200

    stuff

--- common/defines/horst_balance_defines.lua
+++ common/defines/horst_balance_defines.lua
@@ -12,8 +12,8 @@ NDefines.NMilitary.EQUIPMENT_COMBAT_LOSS_FACTOR = 0.875	 	       --  I use this 
 NDefines.NMilitary.AIR_SUPPORT_BASE = 0.25                        -- vanilla 0.25 CAS bonus factor for air support moddifier for land unit in combat
 NDefines.NMilitary.UNIT_DIGIN_CAP = 3                           -- how "deep" you can dig you can dig in until hitting max bonus
 --NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.025			-- vanilla  0.03	-- Amount of planning lost due to player manual order
-NDefines.NMilitary.COMBAT_VALUE_ORG_IMPORTANCE = 1		-- VANILLA 1     Multiplier on TotalOrganisation when determining the combat value of a division
-NDefines.NMilitary.COMBAT_VALUE_STR_IMPORTANCE = 0.15 		-- VANILLA 1      Multiplier on TotalStrength when determining the combat value of a division
+NDefines.NMilitary.COMBAT_VALUE_ORG_IMPORTANCE = -100		-- VANILLA 1     Multiplier on TotalOrganisation when determining the combat value of a division
+NDefines.NMilitary.COMBAT_VALUE_STR_IMPORTANCE = 0.5		-- VANILLA 1      Multiplier on TotalStrength when determining the combat value of a division
 
 NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.5                 -- percentage of experienced solders who die when manpower is removed
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.00035
