c38ac74d4ba862fe72ff89602997827c2237ee73
tresimah <pharmercyaddict@gmail.com>
2021-06-11 23:54:23 +0200

    misc
    -Air losses lowered from 0.44 to 0.42
    -Fixed blowing the suez if Iraq owns the Suez
    -Split the Red Sea into two strategic regions

--- common/defines/horst_graphic_defines.lua
+++ common/defines/horst_graphic_defines.lua
@@ -35,7 +35,6 @@ NDefines_Graphics.NGraphics.PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
 NDefines_Graphics.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 800
 NDefines_Graphics.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 600
 NDefines_Graphics.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 500
-NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 1.0
 NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {200, 350, 500}
 NDefines_Graphics.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 5
 NDefines_Graphics.NGraphics.DRAW_DETAILED_CUTOFF = 500
